6e52bfeb9bbe95aa7857c6f9e1202e177fe1e31e
pedrinho-devv <pedrodesousakiske@gmail.com>
2025-06-17 10:29:39 -0300

    chore: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         {/* Outros meta tags e links podem ser adicionados aqui */}
       </head>
       <body
